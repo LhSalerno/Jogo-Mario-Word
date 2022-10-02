d004f726d99aec170ce10ff358f01c02b5baa363
Luis Salerno <luis.h.salerno@gmail.com>
2022-10-01 23:32:07 -0300

    Finalizado

--- js/javascript.js
+++ js/javascript.js
@@ -12,9 +12,27 @@ const jump = () => {
 
 }
 
-const loop = setInterval(() =>{
+const loop = setInterval(() => {
 
-    const pipePosition = pipe.offsetLefet;
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario) .bottom.replace("px",'');
+
+    console.log(marioPosition)
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${pipePosition}px`;
+
+        mario.src = 'Imagens/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+
+    }
 
 }, 10)
 
